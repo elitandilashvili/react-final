b467685fe3867614b712a2813cf2d3a76dfbe87e
elitandilashvili <elielitandilashvili@gmail.com>
2021-11-13 15:17:36 +0400

    add

--- src/Pages/private-page/PrivatePage.js
+++ src/Pages/private-page/PrivatePage.js
@@ -1,8 +1,10 @@
 export const PrivatePage = () => {
   return (
-    <div className="Login-div">
-      <div>
-        <h4 className="title title-text">This is Main page</h4>
+    <div>
+      <div className="Login-div">
+        <div>
+          <h4 className="title title-text">This is Main page</h4>
+        </div>
       </div>
     </div>
   );
